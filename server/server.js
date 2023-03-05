2d146977d072b2ddfb713dedcfe2b144c2c5f1f9
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-03-05 08:40:17 -0800

    schema

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +19,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
